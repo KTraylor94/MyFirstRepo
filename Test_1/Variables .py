46192abc0d9f93a8af23c7a502a32bc8ae3cf6cc
KTraylor94 <kjtray94@aol.com>
2024-06-12 15:40:43 -0500

    Test1

---
+++ Test_1/Variables .py
@@ -0,0 +1,8 @@
+full_name = "Krista Traylor"
+country_to_visit = "Spain"
+days_to_stay = 7
+exchange_rate = 1.18
+satisfied = True
+print("Hello", full_name)
+print("You will be visiting", country_to_visit, "for", days_to_stay, "days.")
+prnt
